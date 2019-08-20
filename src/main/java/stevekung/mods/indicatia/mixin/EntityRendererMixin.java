a45a2d981e304641cefd0e241646bea0a216e780
SteveKunG <mccommander_minecraft@hotmail.com>
2019-08-20 15:25:58 +0700

    Fixed https://bugs.mojang.com/browse/MC-30845

--- src/main/java/stevekung/mods/indicatia/mixin/EntityRendererMixin.java
+++ src/main/java/stevekung/mods/indicatia/mixin/EntityRendererMixin.java
@@ -3,10 +3,14 @@ package stevekung.mods.indicatia.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import stevekung.mods.indicatia.config.ConfigManagerIN;
 
 @Mixin(EntityRenderer.class)
@@ -29,5 +33,11 @@ public abstract class EntityRendererMixin
         {
             GlStateManager.disableBlend();
         }
+    }
+
+    @Redirect(method = "orientCamera(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;rayTraceBlocks(Lnet/minecraft/util/Vec3;Lnet/minecraft/util/Vec3;)Lnet/minecraft/util/MovingObjectPosition;"), expect = 0)
+    private MovingObjectPosition rayTraceBlocks(WorldClient world, Vec3 from, Vec3 to)
+    {
+        return world.rayTraceBlocks(from, to, false, true, true);
     }
 }
