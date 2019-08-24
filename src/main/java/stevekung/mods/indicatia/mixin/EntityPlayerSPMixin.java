8d1dd23d37f879b042f41ac3d4baf9c900cad760
SteveKunG <mccommander_minecraft@hotmail.com>
2019-08-24 17:50:29 +0700

    Fixed health boost causes hurt camera effect

--- src/main/java/stevekung/mods/indicatia/mixin/EntityPlayerSPMixin.java
+++ src/main/java/stevekung/mods/indicatia/mixin/EntityPlayerSPMixin.java
@@ -1,6 +1,8 @@
 package stevekung.mods.indicatia.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -9,6 +11,7 @@ import com.mojang.authlib.GameProfile;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import stevekung.mods.indicatia.utils.CommonUtils;
 
@@ -16,6 +19,9 @@ import stevekung.mods.indicatia.utils.CommonUtils;
 public abstract class EntityPlayerSPMixin extends AbstractClientPlayer
 {
     private final EntityPlayerSP that = (EntityPlayerSP) (Object) this;
+
+    @Shadow
+    private boolean hasValidHealth;
 
     public EntityPlayerSPMixin(World world, GameProfile profile)
     {
@@ -26,5 +32,36 @@ public abstract class EntityPlayerSPMixin extends AbstractClientPlayer
     private void updateMovementInput(CallbackInfo ci)
     {
         CommonUtils.onInputUpdate(this.that, this.that.movementInput);
+    }
+
+    @Overwrite
+    public void setPlayerSPHealth(float health)
+    {
+        if (this.hasValidHealth)
+        {
+            float f = this.getHealth() - health;
+
+            if (f <= 0.0F)
+            {
+                this.setHealth(health);
+
+                if (f < 0.0F)
+                {
+                    this.hurtResistantTime = this.maxHurtResistantTime / 2;
+                }
+            }
+            else
+            {
+                this.lastDamage = f;
+                this.setHealth(this.getHealth());
+                this.hurtResistantTime = this.maxHurtResistantTime;
+                this.damageEntity(DamageSource.generic, f);
+            }
+        }
+        else
+        {
+            this.setHealth(health);
+            this.hasValidHealth = true;
+        }
     }
 }
