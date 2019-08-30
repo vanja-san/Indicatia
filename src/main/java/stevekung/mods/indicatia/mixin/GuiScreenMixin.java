5a55f8c09274abee927f9b3b847a2bc4bb7ed825
SteveKunG <mccommander_minecraft@hotmail.com>
2019-08-31 00:13:18 +0700

    Fixed https://bugs.mojang.com/browse/MC-2781

--- src/main/java/stevekung/mods/indicatia/mixin/GuiScreenMixin.java
+++ src/main/java/stevekung/mods/indicatia/mixin/GuiScreenMixin.java
@@ -1,10 +1,16 @@
 package stevekung.mods.indicatia.mixin;
 
+import java.io.IOException;
+
+import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 import stevekung.mods.indicatia.utils.GuiChatRegistry;
@@ -12,6 +18,12 @@ import stevekung.mods.indicatia.utils.GuiChatRegistry;
 @Mixin(GuiScreen.class)
 public abstract class GuiScreenMixin extends Gui
 {
+    @Shadow
+    private Minecraft mc;
+
+    @Shadow
+    protected abstract void keyTyped(char typedChar, int keyCode) throws IOException;
+
     @Inject(method = "mouseReleased(III)V", at = @At("RETURN"))
     private void mouseReleased(int mouseX, int mouseY, int mouseButton, CallbackInfo info)
     {
@@ -22,5 +34,17 @@ public abstract class GuiScreenMixin extends Gui
     private void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick, CallbackInfo info)
     {
         GuiChatRegistry.getGuiChatList().forEach(gui -> gui.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick));
+    }
+
+    @Overwrite
+    public void handleKeyboardInput() throws IOException
+    {
+        char c0 = Keyboard.getEventCharacter();
+
+        if (Keyboard.getEventKey() == 0 && c0 >= ' ' || Keyboard.getEventKeyState())
+        {
+            this.keyTyped(c0, Keyboard.getEventKey());
+        }
+        this.mc.dispatchKeypresses();
     }
 }
