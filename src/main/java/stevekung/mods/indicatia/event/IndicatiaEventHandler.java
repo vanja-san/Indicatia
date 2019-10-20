074b1aeba18261e045fe6f2fb2c2625c1eeb713a
SteveKunG <mccommander_minecraft@hotmail.com>
2019-10-20 19:39:53 +0700

    Fixed more toggle sneak bug

--- src/main/java/stevekung/mods/indicatia/event/IndicatiaEventHandler.java
+++ src/main/java/stevekung/mods/indicatia/event/IndicatiaEventHandler.java
@@ -142,7 +142,7 @@ public class IndicatiaEventHandler
         int keyTGCtrl = InfoUtils.INSTANCE.parseInt(keyTS[0], "Toggle Sprint");
         int keyTGOther = InfoUtils.INSTANCE.parseInt(keyTS[1], "Toggle Sprint");
 
-        if (Keyboard.isKeyDown(keyTGCtrl) && Keyboard.isKeyDown(keyTGOther))
+        if (this.mc.gameSettings.keyBindSneak.getKeyCode() != Keyboard.KEY_LCONTROL && keyTGCtrl == Keyboard.KEY_LCONTROL && keyTGOther == Keyboard.KEY_S && Keyboard.isKeyDown(keyTGCtrl) && Keyboard.isKeyDown(keyTGOther))
         {
             ++movement.moveForward;
         }
