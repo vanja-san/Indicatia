ad9511f6e3fb691cac77c72235653bb3102837e4
SteveKunG <mccommander_minecraft@hotmail.com>
2019-10-20 20:50:20 +0700

    Hide Toast when chat is open

--- src/main/java/stevekung/mods/indicatia/mixin/MinecraftMixin.java
+++ src/main/java/stevekung/mods/indicatia/mixin/MinecraftMixin.java
@@ -403,7 +403,10 @@ public abstract class MinecraftMixin
     @Inject(method = "runGameLoop()V", cancellable = true, at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/EntityRenderer.updateCameraAndRender(FJ)V", shift = At.Shift.AFTER))
     private void runGameLoop(CallbackInfo info)
     {
-        HUDRenderEventHandler.INSTANCE.getToastGui().drawToast(new ScaledResolution(this.that));
+        if (!this.that.ingameGUI.getChatGUI().getChatOpen())
+        {
+            HUDRenderEventHandler.INSTANCE.getToastGui().drawToast(new ScaledResolution(this.that));
+        }
     }
 
     private void updateKeyBindState()
